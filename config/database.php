47a3c5e1953eb031c9ea55631ddf0c195f638518
Lucas Yang <yangchenshin77@gmail.com>
2020-09-25 17:38:59 +0800

    Day 27 Update DB connection config for Heroku Postgres

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL = parse_url(getenv('DATABASE_URL'));
+
 return [
 
     /*
@@ -65,17 +67,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL['host'] ?? '127.0.0.1',
+            'port' => $DATABASE_URL['port'] ?? '5432',
+            'database' => ltrim($DATABASE_URL['path'] ?? 'forge', '/'),
+            'username' => $DATABASE_URL['user'] ?? 'forge',
+            'password' => $DATABASE_URL['pass'] ?? '',
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
